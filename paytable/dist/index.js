9bc1dec657ce6a33dd172592b10250bbabc4a65c
shahilalgoo <shahil.algoo@gmail.com>
2025-02-08 16:51:23 +0400

    players in bottom 67% of paid places get their money back

--- paytable/dist/index.js
+++ paytable/dist/index.js
@@ -1,6 +1,29 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const share_based_paytable_1 = require("./share-based-paytable");
+// import { generateSharePaytable } from "./share-based-paytable";
+// const shareBasedPaytable = generateSharePaytable(200, 5000);
 const log_entire_array_1 = require("./utils/log-entire-array");
-const shareBasedPaytable = (0, share_based_paytable_1.generateSharePaytable)(200, 5000);
-(0, log_entire_array_1.logEntireArray)(shareBasedPaytable);
+// Constants
+const prizePoolShare = 0.7; // 70 percent of revenue goes to the prize pool // ❗
+const totalPaidPercentage = 0.3; // 30 percent of total participants will be paid // ❗
+// Inputs
+const ticketPrice = 0.05;
+const totalParticipants = 10000;
+// Variables 
+let totalPrizePool = 0;
+let totalInPaytable = 0;
+let totalPlacesPaid = 0;
+// Calculate prize pool, rounded
+totalPrizePool = prizePoolShare * totalParticipants * ticketPrice;
+// totalPrizePool = Math.round(totalPrizePool);
+// Calculate paid places, rounded down 
+totalPlacesPaid = Math.floor(totalPaidPercentage * totalParticipants);
+// Initialize paytable
+const payTable = new Array(totalPlacesPaid).fill(0);
+// 2/3 of the bottom paytable with get their money back
+const moneyBackTotalPlayers = Math.round((2 / 3) * payTable.length);
+const moneyBackTotal = moneyBackTotalPlayers * ticketPrice;
+for (let i = totalPlacesPaid - moneyBackTotalPlayers; i < totalPlacesPaid; i++) {
+    payTable[i] = ticketPrice;
+}
+(0, log_entire_array_1.logEntireArray)(payTable);
--- paytable/src/index.ts
+++ paytable/src/index.ts
@@ -1,8 +1,38 @@
-import { generateSharePaytable } from "./share-based-paytable";
+// import { generateSharePaytable } from "./share-based-paytable";
+// const shareBasedPaytable = generateSharePaytable(200, 5000);
 import { logEntireArray } from "./utils/log-entire-array";
 
-const shareBasedPaytable = generateSharePaytable(200, 5000);
 
-logEntireArray(shareBasedPaytable);
+// Constants
+const prizePoolShare = 0.7; // 70 percent of revenue goes to the prize pool // ❗
+const totalPaidPercentage = 0.3; // 30 percent of total participants will be paid // ❗
+
+// Inputs
+const ticketPrice = 0.05;
+const totalParticipants = 10000;
+
+// Variables 
+let totalPrizePool = 0;
+let totalPlacesPaid = 0;
+
+// Calculate prize pool, rounded
+totalPrizePool = prizePoolShare * totalParticipants * ticketPrice;
+
+// Calculate paid places, rounded down 
+totalPlacesPaid = Math.floor(totalPaidPercentage * totalParticipants);
+
+// Initialize paytable
+const payTable = new Array(totalPlacesPaid).fill(0);
+
+// 2/3 of the bottom paytable with get their money back
+const moneyBackTotalPlayers = Math.round((2/3) * payTable.length);
+const moneyBackTotal = moneyBackTotalPlayers * ticketPrice;
+
+for (let i = totalPlacesPaid - moneyBackTotalPlayers; i < totalPlacesPaid; i++) {
+    payTable[i] = ticketPrice;
+}
+
+logEntireArray(payTable);
+
 
 
